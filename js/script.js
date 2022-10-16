0f32360694e22ebe6ab828566f50cab68f36241f
Maria Kireeva <mariakmk037@gmail.com>
2022-10-17 02:20:36 +0300

    added slider

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    $('.works_slider').slick({
+        dots:true,
+        slidesToShow: 3,
+        infinite: true, 
+        centerMode: true, 
+        arrows: true,
+    });
+});
+
+$(document).ready(function(){
+    $('.location_slider').slick({
+        slidesToShow: 3,
+        infinite: true, 
+        centerMode: true, 
+        arrows: true,
+    });
+});
