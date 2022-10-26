0ac3841114901f529086f30c493baffa8d2282a1
Maria Kireeva <mariakmk037@gmail.com>
2022-10-26 14:33:54 +0300

    slider js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.works_slider').slick({
+    $('.works__slider').slick({
         dots:true,
         slidesToShow: 3,
         infinite: true, 
@@ -9,10 +9,42 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-    $('.location_slider').slick({
+    $('.location__slider').slick({
         slidesToShow: 3,
         infinite: true, 
         centerMode: true, 
         arrows: true,
+        responsive: [
+          {
+            breakpoint: 769,
+            settings: {
+              slidesToShow: 1,
+            }
+          },
+        ]
     });
 });
+
+$(document).ready(function(){
+    $('.reviews__slider').slick({
+        slidesToShow: 3,
+        infinite: true, 
+        centerMode: true, 
+        arrows: true,
+        responsive: [
+            {
+              breakpoint: 1025,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+              breakpoint: 769,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+        ]
+    });
+});
+
